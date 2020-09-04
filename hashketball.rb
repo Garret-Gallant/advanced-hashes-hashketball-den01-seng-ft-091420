980b457a5c1eb2157bac7f79531b408cbeaf3f45
Garret Gallant <gallanttechnological@gmail.com>
2020-09-04 22:17:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -162,7 +162,8 @@ end
 def player_numbers team_name
   game_hash.each do |home_away, team_properties|
     if team_name == team_properties[:team_name]
-      team_properties[:players].map do |number|
+      team_properties[:players].map do |player|
+        
         
       end
     end
