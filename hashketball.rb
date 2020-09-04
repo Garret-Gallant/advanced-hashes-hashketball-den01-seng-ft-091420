4229a5c308c874cdc821cf7d9cf6234afa2be5ae
Garret Gallant <gallanttechnological@gmail.com>
2020-09-04 21:39:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -134,8 +134,8 @@ end
 def num_points_scored player_name
   game_hash.each do |home_away, team_properties|
     team_properties[:players].each do |player|
+      binding.pry
       
-      
+    end
   end
-  
 end
